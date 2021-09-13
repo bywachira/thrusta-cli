2bd617486400edcf93c1c1e4cd2fe8f51711be13
wachira_Dev <ewachira254@gmail.com>
2021-09-13 08:13:08 +0300

    ⭐  thrusta/core: install systemstat libs

--- status/status.go
+++ status/status.go
@@ -1,1 +1,34 @@
 package status
+
+import (
+	"github.com/mackerelio/go-osstat/memory"
+	"github.com/mackerelio/go-osstat/uptime"
+)
+
+type Statistics struct {
+	SwapAvailable   int64
+	SwapUsed        int64
+	SwapTotal       int64
+	Uptime          int64
+	MemoryUsed      int64
+	MemoryAvailable int64
+	MemoryTotal     int64
+}
+
+func GetSystemStats() Statistics {
+	// swap := sigar.Swap{}
+	uptime, _ := uptime.Get()
+	memory, _ := memory.Get()
+
+	// swap.Get()
+
+	var systemStatistics Statistics = Statistics{
+		// SwapAvailable: swap.Free,
+		// SwapTotal:     swap.Total,
+		// SwapUsed:      swap.Used,
+		Uptime:     uptime.Milliseconds(),
+		MemoryUsed: memory.Total,
+	}
+
+	return systemStatistics
+}
